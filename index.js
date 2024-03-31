8d685d0a66f68dd22e2f1c9ff2f8bcc73b478493
Royal-Aquarius <160124215+Royal-Aquarius@users.noreply.github.com>
2024-03-31 17:08:34 +0500

    chalking

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
 // 1) Computer will generate a random number - Done
 // 2) User input for guessing number - Done
 // 3) Compare user input with computer generated number -Done
 const randomNumber = Math.floor(Math.random() * 6 + 1);
-console.log("Welcome To A Number Guessing Game");
+console.log(chalk.cyanBright("Welcome To A Number Guessing Game"));
 const answers = await inquirer.prompt([
     {
         name: "userGuessedNumber",
         type: "number",
-        message: "Please Guess A Number Between 1-6: ",
+        message: chalk.greenBright("Please Guess A Number Between 1-6: "),
     },
 ]);
 //Ager condition true hai tu wo if ke bracket ko run kera ga
 if (answers.userGuessedNumber === randomNumber) {
-    console.log("Congratulations! You Guessed Right Number");
+    console.log(chalk.blueBright("Congratulations! You Guessed Right Number"));
 }
 else {
-    console.log("You Guessed Wrong Number");
+    console.log(chalk.redBright("Sorry You Guessed A Wrong Number"));
 }
--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@
 
 import inquirer from "inquirer";
 import { right } from "inquirer/lib/utils/readline.js";
+import chalk from "chalk";
 
 // 1) Computer will generate a random number - Done
 
@@ -11,20 +12,20 @@ import { right } from "inquirer/lib/utils/readline.js";
 
 const randomNumber = Math.floor(Math.random() * 6 + 1);
 
-console.log("Welcome To A Number Guessing Game");
+console.log(chalk.cyanBright("Welcome To A Number Guessing Game"));
 
 const answers = await inquirer.prompt([
     {
         name: "userGuessedNumber",
         type: "number",
-        message: "Please Guess A Number Between 1-6: ",
+        message: chalk.greenBright("Please Guess A Number Between 1-6: "),
     },
 ]);
 
 //Ager condition true hai tu wo if ke bracket ko run kera ga
 
 if (answers.userGuessedNumber === randomNumber){
-    console.log("Congratulations! You Guessed Right Number");
+    console.log(chalk.blueBright("Congratulations! You Guessed Right Number"));
 }else{
-    console.log("You Guessed Wrong Number");
+    console.log(chalk.redBright("Sorry You Guessed A Wrong Number"));
 }   
